a25ebb37d528d4e106ba5c122d707e14a7932101
SAR-SA <shawn.a.rather@gmail.com>
2020-09-28 20:24:58 -0400

    class changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,14 @@
 // Requiring our models and passport as we've configured it
+const router = require("express").Router();
 const db = require("../models");
 const passport = require("../config/passport");
 
-module.exports = function(app) {
+router.use("/excercise", excerciseRoutes);
+
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), (req, res) => {
+  router.post("/login", passport.authenticate("local"), (req, res) => {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       email: req.user.email,
@@ -17,7 +19,7 @@ module.exports = function(app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", (req, res) => {
+  router.post("/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
       password: req.body.password
@@ -30,14 +32,9 @@ module.exports = function(app) {
       });
   });
 
-  // Route for logging user out
-  app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", (req, res) => {
+  router.get("/user_data", (req, res) => {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -50,4 +47,5 @@ module.exports = function(app) {
       });
     }
   });
-};
+
+  module.exports = router
